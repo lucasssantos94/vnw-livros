330cd7071013759a6a6c652b66c4f0f88d0d1c07
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-11-16 17:38:08 -0300

    feat(routes): configured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 const App = () => {
-  return <h1>App !! </h1>;
+  return (
+    <>
+      <h1>header</h1>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import App from "./App";
+import HomePage from "./pages/Home";
+import SearchPage from "./pages/Search";
+import BooksPage from "./pages/Books";
+import RegisterPage from "./pages/Register";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="/livros" element={<BooksPage />} />
+          <Route path="/cadastro" element={<RegisterPage />} />
+          <Route path="/pesquisa" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
