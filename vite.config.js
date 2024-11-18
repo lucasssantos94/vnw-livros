44f3063c02812a1064362d30f4161c9c384cf1d5
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-11-18 09:39:09 -0300

    feat: configured alias for folders

--- vite.config.js
+++ vite.config.js
@@ -20,4 +20,12 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
 });
