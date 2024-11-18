a295088c6545e1940cdb4905ede12d38b2e8bbcd
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-11-18 10:23:21 -0300

    feat: created footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,33 @@
+import Container from "../Container";
+import { socialLinks } from "@constants/social-links";
+
+import styles from "./style.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <section className={styles["contact-footer"]}>
+        <Container>
+          <p>4002-8922</p>
+
+          <div className={styles["social-links"]}>
+            {socialLinks.map((link) => (
+              <a key={link.id} href={link.url} target="_blank" rel="noreferrer">
+                <img src={link.icon} alt={link.url} />
+              </a>
+            ))}
+          </div>
+        </Container>
+      </section>
+      <section className={styles["copy-footer"]}>
+        <Container>
+          <p>
+            Layout desenvolvido pela Vai Na Web para fins educativos - 2024{" "}
+          </p>
+        </Container>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/constants/social-links.js
@@ -0,0 +1,33 @@
+import facebookIcon from "@assets/images/icons/facebook.png";
+import twitterIcon from "@assets/images/icons/twitter.png";
+import youtubeIcon from "@assets/images/icons/youtube.png";
+import linkedinIcon from "@assets/images/icons/linkedin.png";
+import instagramIcon from "@assets/images/icons/instagram.png";
+
+export const socialLinks = [
+  {
+    id: 1,
+    url: "https://www.facebook.com/",
+    icon: facebookIcon,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com/",
+    icon: twitterIcon,
+  },
+  {
+    id: 3,
+    url: "https://www.youtube.com/",
+    icon: youtubeIcon,
+  },
+  {
+    id: 4,
+    url: "https://www.linkedin.com/",
+    icon: linkedinIcon,
+  },
+  {
+    id: 5,
+    url: "https://www.instagram.com/",
+    icon: instagramIcon,
+  },
+];
