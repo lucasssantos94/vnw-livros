b62b40b185fe051cbbc0be8834ce70406d4d78fc
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-11-18 13:30:25 -0300

    style: stylized book page

--- src/pages/Books/index.jsx
+++ src/pages/Books/index.jsx
@@ -1,5 +1,24 @@
+import Container from "../../components/Container";
+import Book from "../../components/Book";
+import { books } from "@constants/books";
+import style from "./style.module.scss";
+
 const BooksPage = () => {
-  return <h1>Pagina de Livros</h1>;
+  return (
+    <>
+      <section className={style["s-books"]}>
+        <Container>
+          <h2 className={style["title-section"]}>Livros Doados</h2>
+
+          <div className={style["box-books"]}>
+            {books.map((book) => (
+              <Book key={book.title} book={book} />
+            ))}
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default BooksPage;
