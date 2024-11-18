67e7f7c98f786e54a2de56b7de2ebd625b93f57b
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-11-18 18:59:52 -0300

    feat: add search functionality by book title

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,38 @@
+import { useParams } from "react-router-dom";
+import { books } from "../../constants/books";
+import Book from "../../components/Book";
+import Container from "../../components/Container";
+
 const SearchPage = () => {
-  return <h1>Pagina de Pesquisa</h1>;
+  const { title } = useParams();
+  console.log(title);
+
+  const searchedBooks = books.filter((book) =>
+    book.title
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .includes(
+        title
+          .toLowerCase()
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, ""),
+      ),
+  );
+
+  return (
+    <>
+      <Container>
+        <h2 className="title-section">{`${searchedBooks.length} ${
+          searchedBooks.length > 1 ? "resultados" : "resultado"
+        } para ${title}`}</h2>
+        <div>
+          {searchedBooks &&
+            searchedBooks.map((book) => <Book key={book.title} book={book} />)}
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default SearchPage;
