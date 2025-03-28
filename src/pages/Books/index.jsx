1d3279d95bcc4de32d98857ccd12e2659926e7eb
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-03-28 16:29:13 -0300

    feat: configured book search in api

--- src/pages/Books/index.jsx
+++ src/pages/Books/index.jsx
@@ -1,20 +1,49 @@
 import Container from "@components/Container";
 import Book from "@components/Book";
-import { books } from "@constants/books";
+// import { books } from "@constants/books";
 import style from "./style.module.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const BooksPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      setIsLoading(true);
+      try {
+        const apiUrl = import.meta.env.VITE_API_URL;
+        const response = await axios.get(`${apiUrl}/livros`);
+        setBooks(response.data);
+        console.log(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
   return (
     <>
       <section className={style["s-books"]}>
         <Container>
           <h2 className={style["title-section"]}>Livros Doados</h2>
 
-          <div className={style["box-books"]}>
-            {books.map((book) => (
-              <Book key={book.title} book={book} />
-            ))}
-          </div>
+          {isLoading ? (
+            <div className={style["loading"]}>
+              <h1>Carregando</h1>
+            </div>
+          ) : (
+            <div className={style["box-books"]}>
+              {books.map((book) => (
+                <Book key={book.titulo} book={book} />
+              ))}
+            </div>
+          )}
         </Container>
       </section>
     </>
